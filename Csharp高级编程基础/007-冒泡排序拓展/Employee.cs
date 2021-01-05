7ce39ef1e817f9e0efbcaa23ef0fe040f9f9d322
username <eamil@qq.com>
2021-01-05 12:45:35 +0800

    208-匿名方法（完成）

---
+++ Csharp高级编程基础/007-冒泡排序拓展/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_冒泡排序拓展
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public int Salary { get; private set; }
+
+        public Employee(string name,int salary)
+        {
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+
+        //如果e1大于e2的话，返回true，反之为false
+        public static bool Compare(Employee e1,Employee e2)
+        {
+            if (e1.Salary > e2.Salary) return true;
+            return false;
+            
+        }
+        public override string ToString()
+        {
+            return Name+":"+Salary;
+        }
+    }
+}
---
+++ Csharp高级编程基础/008-多播委托/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_多播委托
+{
+    class Program
+    {
+        static void Test1()
+        {
+            Console.WriteLine("Test1");
+        }
+
+        static void Test2()
+        {
+            Console.WriteLine("Test2");
+        }
+
+        static void Main(string[] args)
+        {
+            //多播委托，引用多个方法的委托为多播委托
+            Action a = Test1;
+            a += Test2;//表示添加一个委托的引用
+
+            //a -= Test1;
+            //a -= Test2;
+
+
+            //if (a!=null)
+            //a();//当一个委托没有指向任何方法的时候，调用的话会出现异常null
+
+            Delegate[] delegates = a.GetInvocationList();
+            foreach (Delegate de in delegates)
+            {
+                de.DynamicInvoke();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

0e4becc315f3445852626087dae9d1bedb5ba6f4
username <eamil@qq.com>
2021-01-06 15:06:27 +0800

    212-观察者设计模式-委托与事件的区别（完成）

---
+++ Csharp高级编程基础/012-观察者设计模式-猫捉老鼠/Mouse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012_观察者设计模式_猫捉老鼠
+{
+    //老鼠为观察者，观察者类
+    class Mouse
+    {
+        private string name;
+        private string color;
+
+        public Mouse(string name,string color,Cat cat)
+        {
+            this.name = name;
+            this.color = color;
+            cat.catCome += this.RunAway;//把自身的逃跑方法 注册进 猫里面 订阅消息
+        }
+
+        //逃跑功能
+        public void RunAway()
+        {
+            Console.WriteLine(color+"的老鼠"+name+"说：老猫来了，赶紧跑，我跑，我使劲跑，我加速使劲跑。。。");
+        }
+
+    }
+}
